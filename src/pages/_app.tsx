22201a0c17ae9593b4fba2ac0cc261232b72ee4a
Noah <kangyongsuek@gmail.com>
2023-09-25 23:47:42 +0900

    add: dayjs 패키지 추가

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,11 @@ import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 
 import { Provider } from 'jotai';
+import dayjs from 'dayjs';
+
+import 'dayjs/locale/ko'
+
+dayjs.locale('ko')
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
