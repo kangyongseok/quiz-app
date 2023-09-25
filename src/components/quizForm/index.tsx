56de9d4967b03b20ca5c46c1c628a0a5cd8cd3b4
Noah <kangyongsuek@gmail.com>
2023-09-25 15:42:44 +0900

    feat: 퀴즈 실제 api 호출 및 데이터 적용

--- src/components/quizForm/index.tsx
+++ src/components/quizForm/index.tsx
@@ -1,29 +1,29 @@
-import CheckBox from "../checkBox";
+import type { QuizFormType } from "@/types/quiz";
 
-interface QuizFormProps {
-  question: string;
-  answerList: string[];
-  answer: string;
-}
+import RadioBox from "../radioBox";
 
 function QuizForm({
   question,
   answerList,
-  answer
-}: QuizFormProps) {
+  answer,
+  onClick
+}: QuizFormType) {
   return (
     <div className="flex_screen_center flex-col bg-gradient-to-r from-cyan-500 to-blue-500">
       <div className="max-w-sm rounded overflow-hidden shadow-lg px-6 py-4 bg-white">
-        <p className="font-bold text-2xl pb-2">{question}</p>
-        <ol>
+        <p className="font-bold text-2xl pb-2" dangerouslySetInnerHTML={{__html: question}} />
+        <ul>
           {
-            answerList.map(list => (
+            answerList?.map(list => (
               <li key={`answer-${list}`} className="mt-1">
-                <CheckBox label={list} />
+                <RadioBox label={list} />
               </li>
             ))
           }
-        </ol>
+        </ul>
+        <button className="bg-blue-500 text-white font-bold py-2 px-4 rounded mt-4 disabled:opacity-25" disabled onClick={onClick}>
+          다음
+        </button>
       </div>
     </div>
   )
--- src/pages/quiz/index.tsx
+++ src/pages/quiz/index.tsx
@@ -1,14 +1,34 @@
+import { Suspense, useMemo, useState } from "react"
+
 import QuizForm from "@/components/quizForm"
 
+import { useQuery } from "@/hooks/useQuery"
+
+import { shuffle } from "@/utils/shuffle";
+
 function Quiz() {
-  const sample = {
-    question: '질문',
-    answerList: ['답1', '답2', '답3', '답4'],
-    answer: '답1'
+  const [currentQuiz, setCurrentQuiz] = useState(0);
+  const {data, isLoading} = useQuery({ url: 'https://opentdb.com/api.php?amount=10&difficulty=medium&type=multiple' })
+
+
+  const quizData = useMemo(() => {
+    return data.map(quiz => ({
+      question: quiz.question,
+      answerList: shuffle([...quiz.incorrect_answers, quiz.correct_answer]),
+      answer: quiz.correct_answer
+    }))
+  }, [data])
+
+  const handleClickNext = () => {
+    setCurrentQuiz(prevQuiz => prevQuiz + 1);
   }
 
   return (
-    <QuizForm {...sample} />
+    <Suspense fallback={<div>Loading...</div>}>
+      {
+        data && <QuizForm {...quizData[currentQuiz]} onClick={handleClickNext} />
+      }
+    </Suspense>
   )
 }
 
