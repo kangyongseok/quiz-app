de3d2950c2fba6c0d80a14f787334a760c3dc6f1
Noah <kangyongsuek@gmail.com>
2023-09-26 00:10:33 +0900

    add: 차트 라이브러리 추가 및 차트 구현

--- src/components/quizResultCorrectChart/index.tsx
+++ src/components/quizResultCorrectChart/index.tsx
@@ -1,8 +1,25 @@
-import React from 'react'
+import { Pie } from "react-chartjs-2"
+import { useAtomValue } from "jotai";
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
+
+import { quizResultAtom } from "@/jotai/quiz";
+
+ChartJS.register(ArcElement, Tooltip, Legend)
 
 function QuizResultCorrectChart() {
+  const quizResult = useAtomValue(quizResultAtom)
   return (
-    <div>index</div>
+    <Pie data={{
+      labels: ['정답', '오답'],
+      datasets: [{
+        label: '퀴즈 풀이',
+        data: [quizResult.correctQuiz.length, quizResult.incorrectQuiz.length],
+        backgroundColor: [
+          'rgb(54, 162, 235)',
+          'rgb(255, 99, 132)'
+        ]
+      }]
+    }} />
   )
 }
 
