6765635b8d019aa31ac867f75406e785c5e2486e
Noah <kangyongsuek@gmail.com>
2023-09-25 14:32:21 +0900

    design: 퀴즈 결과 페이지 스타일 추가

--- src/pages/quizResult/index.tsx
+++ src/pages/quizResult/index.tsx
@@ -2,22 +2,24 @@ import React from 'react'
 
 function QuizResult() {
   return (
-    <div>
-      <div className='flex'>
-        <p>퀴즈 풀이 시간: </p>
-        <p>10분</p>
-      </div>
-      <div className='flex'>
-        <p>총 퀴즈 갯수</p>
-        <p>10</p>
-      </div>
-      <div className='flex'>
-        <p>정답</p>
-        <p>3</p>
-      </div>
-      <div className='flex'>
-        <p>오답</p>
-        <p>10분</p>
+    <div className='flex_screen_center'>
+      <div className="text-center flex flex-col gap-3">
+        <div>
+          <p>퀴즈 풀이 시간</p>
+          <p>10분</p>
+        </div>
+        <div>
+          <p>총 퀴즈 갯수</p>
+          <p className='font-bold'>10</p>
+        </div>
+        <div>
+          <p>정답</p>
+          <p className="text-blue-500 font-bold">3</p>
+        </div>
+        <div>
+          <p>오답</p>
+          <p className="text-red-500 font-bold">10분</p>
+        </div>
       </div>
     </div>
   )
