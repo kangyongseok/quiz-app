143b3f0431e72566d5a5e3c5511a057d2683753e
Noah <kangyongsuek@gmail.com>
2023-09-25 23:53:47 +0900

    feat: 다음문제 풀기 관련 비즈니스 로직 추가

--- src/pages/quiz/index.tsx
+++ src/pages/quiz/index.tsx
@@ -1,14 +1,30 @@
 import { Suspense, useMemo, useState } from "react"
 
+import { useRouter } from "next/router";
+
+import { useResetAtom } from 'jotai/utils'
+import { useAtomValue, useSetAtom } from "jotai";
+import dayjs from "dayjs";
+
 import QuizForm from "@/components/quizForm"
 
 import { useQuery } from "@/hooks/useQuery"
 
+import { QUIZ_COUNT } from "@/constants/quiz";
+
 import { shuffle } from "@/utils/shuffle";
+import { endTimeAtom, quizResultAtom, selectedAnswerAtom } from "@/jotai/quiz";
 
 function Quiz() {
+  const router = useRouter();
   const [currentQuiz, setCurrentQuiz] = useState(0);
-  const {data, isLoading} = useQuery({ url: 'https://opentdb.com/api.php?amount=10&difficulty=medium&type=multiple' })
+
+  const answer = useAtomValue(selectedAnswerAtom)
+  const setAnswerResult = useSetAtom(quizResultAtom);
+  const setEndTime = useSetAtom(endTimeAtom)
+  const resetSelectedAnswer = useResetAtom(selectedAnswerAtom)
+
+  const {data, isLoading} = useQuery({ url: `https://opentdb.com/api.php?amount=${QUIZ_COUNT}&difficulty=medium&type=multiple` })
 
 
   const quizData = useMemo(() => {
@@ -19,8 +35,29 @@ function Quiz() {
     }))
   }, [data])
 
+  const setQuizResultData = () => {
+    if (answer === quizData[currentQuiz].answer) {
+      setAnswerResult((prevData) => ({
+        ...prevData,
+        correctQuiz: [...prevData.correctQuiz, quizData[currentQuiz]]
+      }))
+    } else {
+      setAnswerResult((prevData) => ({
+        ...prevData,
+        incorrectQuiz: [...prevData.incorrectQuiz, quizData[currentQuiz]]
+      }))
+    }
+  }
+
   const handleClickNext = () => {
-    setCurrentQuiz(prevQuiz => prevQuiz + 1);
+    resetSelectedAnswer()
+    setQuizResultData()
+    if (currentQuiz + 1 < QUIZ_COUNT) {
+      setCurrentQuiz(prevQuiz => prevQuiz + 1);
+    } else {
+      setEndTime(dayjs())
+      router.push('/quizResult')
+    }
   }
 
   return (
