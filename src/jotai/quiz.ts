4001f54ab7ed3b6a6da27cdb2ffad071b6af158f
Noah <kangyongsuek@gmail.com>
2023-09-25 23:51:35 +0900

    add: 전역상태값 추가

--- src/jotai/quiz.ts
+++ src/jotai/quiz.ts
@@ -1,3 +1,14 @@
-import { atom } from "jotai";
+import { atomWithReset, atomWithStorage } from "jotai/utils";
+import dayjs from "dayjs";
 
-export const selectedAnswerAtom = atom('')
+import { type QuizResultType } from "@/types/quiz";
+
+export const selectedAnswerAtom = atomWithReset('')
+
+export const startTimeAtom = atomWithStorage('quizStart', dayjs())
+export const endTimeAtom = atomWithStorage('quizEnd', dayjs())
+
+export const quizResultAtom = atomWithStorage<QuizResultType>('quizResult', {
+  correctQuiz: [],
+  incorrectQuiz: []
+})
