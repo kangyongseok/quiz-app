44f0bc69d3ec86e50490fcfdf6d245976d6e305c
Noah <kangyongsuek@gmail.com>
2023-09-26 03:27:34 +0900

    add: 테스트 샘플코드 작성'

---
+++ __tests__/index.test.tsx
@@ -0,0 +1,9 @@
+import { render, screen } from '@testing-library/react';
+import Home from '@/pages/index'
+
+describe('Home', () => {
+  it('renders a button', () => {
+    const { getByText } = render(<Home />)
+    expect(getByText('퀴즈 풀기')).toBeInTheDocument()
+  })
+})
--- jest.config.ts
+++ jest.config.ts
@@ -8,8 +8,8 @@ const createJestConfig = nextJest({
 
 // Add any custom config to be passed to Jest
 const customJestConfig = {
-  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
-  testEnvironment: 'jest-environment-jsdom'
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  testEnvironment: 'jsdom'
 };
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
