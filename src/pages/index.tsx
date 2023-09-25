c61de5134abcdd97fb9c02cc5cb12c02c2cff637
Noah <kangyongsuek@gmail.com>
2023-09-25 11:06:09 +0900

    feat: 퀴즈풀기 버튼 UI 추가

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,16 @@
+import Link from 'next/link'
 import { Inter } from 'next/font/google'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <div className={`flex items-center content-center ${inter.className}`}>
-      <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+    <div className={`flex items-center justify-center ${inter.className} w-screen h-screen`}>
+      <Link href="/quiz">
+        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
         퀴즈 풀기
-      </button>
+        </button>
+      </Link>
     </div>
   )
 }
