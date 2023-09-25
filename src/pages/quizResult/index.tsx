bc8d6164c041b7c7016a3e817f97ee8e57c7f764
Noah <kangyongsuek@gmail.com>
2023-09-25 23:53:06 +0900

    feat: 퀴즈결과 데이터 노출 상세 코드 추가

--- src/pages/quizResult/index.tsx
+++ src/pages/quizResult/index.tsx
@@ -1,24 +1,37 @@
-import React from 'react'
+import { useAtomValue } from 'jotai'
+
+import { QUIZ_COUNT } from '@/constants/quiz'
+
+import { getDiffTimeString } from '@/utils/getDiffTimeString'
+import { endTimeAtom, quizResultAtom, startTimeAtom } from '@/jotai/quiz'
 
 function QuizResult() {
+  const startTime = useAtomValue(startTimeAtom)
+  const endTime = useAtomValue(endTimeAtom)
+  const quizResult = useAtomValue(quizResultAtom)
+
   return (
     <div className='flex_screen_center'>
       <div className="text-center flex flex-col gap-3">
         <div>
           <p>퀴즈 풀이 시간</p>
-          <p>10분</p>
+          <p>{getDiffTimeString(startTime, endTime)}</p>
         </div>
         <div>
           <p>총 퀴즈 갯수</p>
-          <p className='font-bold'>10</p>
+          <p className='font-bold'>{QUIZ_COUNT}</p>
         </div>
         <div>
           <p>정답</p>
-          <p className="text-blue-500 font-bold">3</p>
+          <p className="text-blue-500 font-bold">
+            {quizResult.correctQuiz.length}
+          </p>
         </div>
         <div>
           <p>오답</p>
-          <p className="text-red-500 font-bold">10분</p>
+          <p className="text-red-500 font-bold">
+            {quizResult.incorrectQuiz.length}
+          </p>
         </div>
       </div>
     </div>
